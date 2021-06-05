acc7509190329b7ec956c10fccfcd0791a959aaa
Marco Beretta <marco.beretta@schmatt.it>
2021-06-05 16:28:54 +0200

    commit

---
+++ components/layout.js
@@ -0,0 +1,6 @@
+const name = '[Marco]'
+
+export default function Layout({ children }) {
+    return <div>{children}</div>
+  }
+  
---
+++ pages/posts/first-post.js
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+const YourComponent = () => (
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+)
+
+
+
+export default function FirstPost(props) {
+  console.log("PROPS", props)
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
+
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  // const res = await fetch(`https://.../data`)
+  const data = {
+    id: "hsdjsdkh",
+    name: "Marco",
+    surname: "Beretta"
+  }
+
+  // Pass data to the page via props
+  return { props: data }
+}
